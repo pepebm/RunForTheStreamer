d81186cbeb4bde224be137254521ecfa2b8b46b3
Jose Manuel Beauregard Mendez <jmbeauregard@ymail.com>
2018-09-21 17:13:22 -0500

    Player sprite & controller

---
+++ Assets/Scripts/HelloMixer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloMixer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MixerInteractive.GoInteractive();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (MixerInteractive.GetButton("Up"))
+        {
+            Debug.Log("Player pressed up");
+        }
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed, jumpForce;
+    private Rigidbody2D body;
+
+
+	// Use this for initialization
+	void Start () {
+        body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        body.velocity = new Vector2(moveSpeed, body.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            body.velocity = new Vector2(body.velocity.x , jumpForce);
+        }
+	}
+}
